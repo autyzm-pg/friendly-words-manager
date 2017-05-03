3ca1da46ed2a9c0ec951258731a20512bcc637b6
Klaudiusz Dembler <k.dembler@gmail.com>
2017-05-03 20:09:28 +0200

    Initial implementation of active config

--- app/actions/configurationsActions.js
+++ app/actions/configurationsActions.js
@@ -1,3 +1,3 @@
 
-
 export const changeConfigsSearchQuery = (newQuery) => ({type: "CONFIGURATIONS_LIST_QUERY_CHANGE", payload: newQuery})
+export const changeActiveConfig = (activeConfig) => ({type: "CONFIGURATIONS_LIST_ACTIVE_CONFIG_CHANGE", payload: activeConfig})
--- app/reducers/configurationsReducer.js
+++ app/reducers/configurationsReducer.js
@@ -11,11 +11,13 @@ const defaultState = {
         "Jasiu Stasiu",
         "Zdzisiu Misiu"
     ],
-    searchQuery: ""
+    searchQuery: "",
+    active: "Konfiguracja 1"
 }
 
 const handlers = {
-    "CONFIGURATIONS_LIST_QUERY_CHANGE": (state, action) => R.assoc('searchQuery', action.payload.toLowerCase(), state)
+    "CONFIGURATIONS_LIST_QUERY_CHANGE": (state, action) => R.assoc('searchQuery', action.payload.toLowerCase(), state),
+    "CONFIGURATIONS_LIST_ACTIVE_CONFIG_CHANGE": (state, action) => R.assoc('active', action.payload, state)
 }
 
 export default createReducer(defaultState,handlers)
