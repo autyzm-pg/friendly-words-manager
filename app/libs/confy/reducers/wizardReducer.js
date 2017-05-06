67fd860eb90d3cb665c75c8c24c38326d93bb0f5
Wojciech <w.nadurski@gmail.com>
2017-05-06 18:36:50 +0200

    Removed unnecessary defaultState in wizardReducer

--- app/libs/confy/reducers/wizardReducer.js
+++ app/libs/confy/reducers/wizardReducer.js
@@ -1,17 +1,13 @@
 import {getDefaultModel} from "../fields"
-const defaultState = {
-    model: undefined,
-    activeConfig: {}
-}
+
 
 export const wizardReducer = model => {
-    const newDefaultState = {
-        ...defaultState,
+    const defaultState = {
         model: model,
         activeConfig: getDefaultModel(model)
     }
 
-    return (state = newDefaultState, action) => {
+    return (state = defaultState, action) => {
         return state
     }
 }
