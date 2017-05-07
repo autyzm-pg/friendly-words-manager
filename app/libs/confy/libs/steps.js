dbe33fc0ff50df9c35a8709289db97c45d487697
Wojciech <w.nadurski@gmail.com>
2017-05-07 12:34:09 +0200

    Removing unnecesary files. Renaming component StepView to StepListView

--- app/libs/confy/libs/steps.js
+++ app/libs/confy/libs/steps.js
@@ -1,3 +1,3 @@
-import StepView from "../components/steps/StepListView"
-export const Step = (name, fields) => ({type: "Step", name, fields, component: StepView})
-export const FullStep = (name, field) => ({type: "Step", name, fields: [field], component: StepView})
+import StepListView from "../components/steps/StepListView"
+export const Step = (name, fields) => ({type: "Step", name, fields, component: StepListView})
+export const FullStep = (name, field) => ({type: "Step", name, fields: [field], component: StepListView})
--- app/libs/confy/steps.js
+++
@@ -1,3 +0,0 @@
-import StepView from "./components/StepView"
-export const Step = (name, fields) => ({type: "Step", name, fields, component: StepView})
-export const FullStep = (name, field) => ({type: "Step", name, fields: [field], component: StepView})
