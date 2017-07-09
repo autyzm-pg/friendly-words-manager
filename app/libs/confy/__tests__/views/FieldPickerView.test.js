e6c022d4be5fcba8458065560f184b4579c203c9
Wojciech Nadurski <wojciech.nadurski@oke.pl>
2017-07-09 10:19:46 +0200

    Field creation improvement

--- app/libs/confy/__tests__/views/FieldPickerView.test.js
+++ app/libs/confy/__tests__/views/FieldPickerView.test.js
@@ -4,10 +4,11 @@ import React from 'react'
 import renderer from "react-test-renderer"
 import FieldPickerView from "../../components/fields/FieldPickerView";
 
-
-it("renders at all", () => {
-    const tree = renderer.create(
-        <FieldPickerView value="" onChange={()=>undefined} verbose="" options={[]} />
-    ).toJSON()
-    expect(tree).toBeTruthy();
-})
+describe('FieldPickerView component', () => (
+    it("renders at all", () => {
+        const tree = renderer.create(
+            <FieldPickerView value="" onChange={() => undefined} verbose="" options={[]}/>
+        ).toJSON()
+        expect(tree).toBeTruthy();
+    })
+))
--- app/libs/confy/libs/fields.js
+++ app/libs/confy/libs/fields.js
@@ -3,30 +3,31 @@ import FieldPickerView from "../components/fields/FieldPickerView"
 import FieldSimpleView from "../components/fields/FieldSimpleView"
 import {notImplementedFunc} from "../../funcs"
 
-export const Field = (type, defaultSettings = {}) => (settings) => ({
-    type,
-    def: "",
-    verbose: "",
-    component: FieldSimpleView,
-    ...defaultSettings,
-    ...R.dissoc("type", settings)
+export const Field = (defaultSettings = {}, component=FieldSimpleView) => (settings) => ({
+    component,
+    props: {
+        def: undefined,
+        verbose: "",
+        ...defaultSettings,
+        ...settings
+    }
 })
 
-export const TextField = Field("Text")
-export const OptionField = Field("Option", {def: 0, options: [], component: FieldPickerView})
-export const BoolField = Field("Bool", {def: false})
-export const ModelListField = Field("ModelList", {def: [], model: undefined})
-export const ChecklistField = Field("Checklist", {def: [], options: []})
-export const ImageChecklistField = Field("ImageCheckList", {def: [], options: []})
-export const ArrayField = notImplementedFunc
-export const ObjectField = notImplementedFunc
-export const ForeignField = notImplementedFunc
+export const TextField = Field()
+export const OptionField = Field({def: 0, options: []}, FieldPickerView)
+export const BoolField = Field({def: false})
+export const ModelListField = Field({def: [], model: undefined})
+export const ChecklistField = Field({def: [], options: []})
+export const ImageChecklistField = Field({def: [], options: []})
+export const ArrayField = Field({def: []})
+export const ObjectField = Field({def: {}})
+export const ForeignField = Field()
 export const Select = notImplementedFunc
-export const MultiOptionField = notImplementedFunc
-export const UriField = notImplementedFunc
-export const IntegerField = notImplementedFunc
-export const MultiImageOptionField = notImplementedFunc
-export const MultiColorField = notImplementedFunc
-export const MultiSizeField = notImplementedFunc
+export const MultiOptionField = Field({options: []})
+export const UriField = Field({def: ""})
+export const IntegerField = Field({def: 0})
+export const MultiImageOptionField = Field({options: []})
+export const MultiColorField = Field({options: []})
+export const MultiSizeField = Field({options: []})
 
 export const getDefaultModel = R.map(field => field.def)
