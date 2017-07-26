53dac66f0f46e2780fa3d8d1fabfba5e53be775e
Wojciech <w.nadurski@gmail.com>
2017-07-26 21:24:25 +0200

    Limiting view and model to small amount of fields to validation

--- app/config/model.js
+++ app/config/model.js
@@ -17,43 +17,54 @@ export const WordModel = Model({
 })
 
 export const ConfigurationModel = Model({
-    materials: ArrayField(ObjectField({
-        word: ForeignField(WordModel, Select({
-            name: TextField({readonly: true}),
-            images: MultiOptionField()
-        })),
-        isInLearningMode: BoolField(),
-        isInTestingMode: BoolField()
-    })),
-    hintType: OptionField([
-        "Wyszarz",
-        "TAK",
-        "Powieksz",
-        "Brak"
-    ]),
-    commandText: OptionField([
-        "Pokaz gdzie jest {slowo}",
-        "{slowo}",
-    ]),
-    picturesNumber: IntegerField(1, 6),
-    isTextForPicture: BoolField(),
-    isReadingCommands: BoolField(),
-    showHintAfter: IntegerField(1, 20),
-    numberOfRepetitions: IntegerField(1, 20),
-    textRewards: MultiOptionField([
-        "Super",
-        "TAK",
-        "SWIETNIE"
-    ]),
-    isReadingRewards: BoolField(),
-    animationRewards: MultiImageOptionField([
-        "image/path"
-    ]),
-    testConfig: ObjectField({
-        numberOfRepetitions: IntegerField(1, 20),
-        timeForAnswer: IntegerField(1, 10)
+    someText: TextField(),
+    someOptionField: OptionField({
+        options: [
+            "option1",
+            "option2"
+        ]
+
     })
 })
+
+// export const ConfigurationModel = Model({
+//     materials: ArrayField(ObjectField({
+//         word: ForeignField(WordModel, Select({
+//             name: TextField({readonly: true}),
+//             images: MultiOptionField()
+//         })),
+//         isInLearningMode: BoolField(),
+//         isInTestingMode: BoolField()
+//     })),
+//     hintType: OptionField([
+//         "Wyszarz",
+//         "TAK",
+//         "Powieksz",
+//         "Brak"
+//     ]),
+//     commandText: OptionField([
+//         "Pokaz gdzie jest {slowo}",
+//         "{slowo}",
+//     ]),
+//     picturesNumber: IntegerField(1, 6),
+//     isTextForPicture: BoolField(),
+//     isReadingCommands: BoolField(),
+//     showHintAfter: IntegerField(1, 20),
+//     numberOfRepetitions: IntegerField(1, 20),
+//     textRewards: MultiOptionField([
+//         "Super",
+//         "TAK",
+//         "SWIETNIE"
+//     ]),
+//     isReadingRewards: BoolField(),
+//     animationRewards: MultiImageOptionField([
+//         "image/path"
+//     ]),
+//     testConfig: ObjectField({
+//         numberOfRepetitions: IntegerField(1, 20),
+//         timeForAnswer: IntegerField(1, 10)
+//     })
+// })
 
 // const LinesConfigurationModel = Model({
 //     materials: ArrayField(ObjectField({
--- app/config/view.js
+++ app/config/view.js
@@ -1,38 +1,47 @@
 import {ConfigurationModel} from "./model"
 import {Column, ColumnView, Section, SectionView, WizardStep, WizardView} from "../libs/confy/libs/steps"
 
-const DetailedListView = notImplementedFunc
-const CustomTestView = notImplementedFunc
+// const DetailedListView = notImplementedFunc
+// const CustomTestView = notImplementedFunc
 
-export const ConfigurationWizardView = WizardView(fields => [
-    WizardStep("Material", DetailedListView(fields.materials)),
-    WizardStep("Sposob Uczenia", SectionView([
-            Section("Ustawienia kroku", [
-                fields.commandText,
-                fields.picturesNumber,
-                fields.isTextForPicture,
-                fields.isReadingCommands
-            ]),
-            Section("Ustawienia proby", [
-                fields.numberOfRepetitions
-            ]),
-            Section("Ustawienia podpowiedzi", [
-                fields.hintType,
-                fields.showHintAfter
-            ])
-        ])
-    ),
-    WizardStep("Wzmocnienia", ColumnView([
-        Column([
-            fields.textRewards,
-            fields.isReadingRewards
-        ]),
-        Column([
-            fields.animationRewards
-        ])
+export const ConfiguratioNWizardView = WizardView(fields => [
+    WizardStep("Step 1", ColumnView([
+        fields.someText
     ])),
-    WizardStep("Test", CustomTestView([
-        fields.testConfig.fields.numberOfRepetitions,
-        fields.testConfig.fields.timeForAnswer
+    WizardStep("Step 2", ColumnView([
+        fields.someOptionField
     ]))
 ], ConfigurationModel)
+
+// export const ConfigurationWizardView = WizardView(fields => [
+//     WizardStep("Material", DetailedListView(fields.materials)),
+//     WizardStep("Sposob Uczenia", SectionView([
+//             Section("Ustawienia kroku", [
+//                 fields.commandText,
+//                 fields.picturesNumber,
+//                 fields.isTextForPicture,
+//                 fields.isReadingCommands
+//             ]),
+//             Section("Ustawienia proby", [
+//                 fields.numberOfRepetitions
+//             ]),
+//             Section("Ustawienia podpowiedzi", [
+//                 fields.hintType,
+//                 fields.showHintAfter
+//             ])
+//         ])
+//     ),
+//     WizardStep("Wzmocnienia", ColumnView([
+//         Column([
+//             fields.textRewards,
+//             fields.isReadingRewards
+//         ]),
+//         Column([
+//             fields.animationRewards
+//         ])
+//     ])),
+//     WizardStep("Test", CustomTestView([
+//         fields.testConfig.fields.numberOfRepetitions,
+//         fields.testConfig.fields.timeForAnswer
+//     ]))
+// ], ConfigurationModel)
