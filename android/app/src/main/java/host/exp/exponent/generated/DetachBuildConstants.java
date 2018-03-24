3f95a7237801331538b7101918c4425071d1c52c
Wojciech <w.nadurski@gmail.com>
2018-03-24 08:40:48 +0100

    Fixing max dex limit. Added development url. Working.

--- android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -3,6 +3,6 @@ package host.exp.exponent.generated;
 // This file is auto-generated. Please don't rename!
 public class DetachBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "";
+  public static final String DEVELOPMENT_URL = "exp6d76f4ee07e14ac099e9c911bdb3ee0f://192.168.0.213:19000";
 
 }
