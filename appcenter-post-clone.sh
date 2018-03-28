aa1a67951171a955c361bf60845978148844cde2
Wojciech Nadurski <wnadurski@atlassian.com>
2018-03-28 22:26:42 +0200

    Fixing name of the API_KEY env variable, adding +x for exp prebuild script

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -6,9 +6,9 @@ echo "Starting post clone script..."
 SOURCE=`dirname $0`
 
 # Add ms appcenter api key
-if [[ ! -z "${APPCENTER_API_KEY}" ]]; then
+if [[ ! -z "${API_KEY}" ]]; then
     echo "Injecting APPCENTER_API_KEY"
-    echo "{ \"app_secret\": \"$APPCENTER_API_KEY\" }" > $SOURCE/android/app/src/main/assets/appcenter-config.json
+    echo "{ \"app_secret\": \"$API_KEY\" }" > $SOURCE/android/app/src/main/assets/appcenter-config.json
 fi
 
 # Set up development URL for expo
