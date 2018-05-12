adb64f5784048853617792175742ec44574a848a
Wojciech <w.nadurski@gmail.com>
2018-05-12 14:18:34 +0200

    Styling resources list

--- app/components/resources/ResourcesList.js
+++ app/components/resources/ResourcesList.js
@@ -1,14 +1,17 @@
 import React from "react"
-import {List, ListItem, Right, View} from "native-base"
+import {Body, Left, List, ListItem, Right, View} from "native-base"
 import {TouchableOpacity} from "react-native"
+import {ListLabel, ListLabelsContainer} from "../../libs/confy/components/ui/ListLabels"
 
 export const ResourceElem = ({item, onElemClick = () => null, children}) => (
     <ListItem>
+        <Body>
         <TouchableOpacity onPress={onElemClick}>
-            <View style={{flex: 1}}>
+            <View style={{flex: 1, marginLeft: 20}}>
                 {item}
             </View>
         </TouchableOpacity>
+        </Body>
         <Right>
             {children}
         </Right>
@@ -16,6 +19,10 @@ export const ResourceElem = ({item, onElemClick = () => null, children}) => (
 )
 export const ResourceList = ({children, onSearchChange, searchQuery}) => (
     <View>
+        <ListLabelsContainer>
+            <Left><ListLabel text={"nazwa zasobu"}/></Left>
+            <Right><ListLabel text={"akcje"}/></Right>
+        </ListLabelsContainer>
         <List style={{marginBottom: 60}}>
             {children}
         </List>
