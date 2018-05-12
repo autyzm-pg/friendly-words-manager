8b6b4671277d6c5b67fb3a94d860392ad203f624
Wojciech <w.nadurski@gmail.com>
2018-05-12 14:12:50 +0200

    Styling listPage

--- app/components/resources/ListPage.js
+++ app/components/resources/ListPage.js
@@ -1,5 +1,20 @@
 import React from "react"
-import {Body, Button, Container, Content, Header, Icon, Left, Right, Title} from "native-base"
+import {Body, Button, Container, Content, Header, Icon, Left, Right, Title, View} from "native-base"
+import {withStyle} from "../../libs/withStyle"
+import {StyleSheet} from "react-native"
+
+const scrollStyles = StyleSheet.create({
+    scrollContainer: {
+        flexDirection: "row",
+        justifyContent: "center",
+    }
+})
+
+const ContentContainer = withStyle({
+    maxWidth: 800,
+    marginTop: 20,
+    flex: 1,
+})(View)
 
 export const ListPage = ({onBack, title, rightContent, children}) =>
     <Container>
@@ -17,8 +32,10 @@ export const ListPage = ({onBack, title, rightContent, children}) =>
             </Right>
         </Header>
 
-        <Content keyboardShouldPersistTaps="handled">
-            {children}
+        <Content contentContainerStyle={scrollStyles.scrollContainer} keyboardShouldPersistTaps="handled">
+            <ContentContainer>
+                {children}
+            </ContentContainer>
         </Content>
     </Container>
 
