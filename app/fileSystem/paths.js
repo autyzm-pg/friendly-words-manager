3e445f1e8f56296ecf34ac60304e92a74f380da2
Wojciech Nadurski <wnadurski@atlassian.com>
2018-09-05 15:31:29 +0200

    linking react-native-vector-icon - this should probably be done earlier
    Adding and injecting initial assets bundle

--- app/fileSystem/paths.js
+++ app/fileSystem/paths.js
@@ -1,10 +1,10 @@
 import {ExternalStorageDirectoryPath} from "react-native-fs"
 
 const path = ExternalStorageDirectoryPath;
-const appDirectory = "friendly-words-app";
 const dbName = "db.json";
 const assetsDirectory = "assets";
 
+export const appDirectory = "friendly-words-app";
 export const appDataPath = `${path}/${appDirectory}`
 export const configsDatabase = `${appDataPath}/${dbName}`
 export const uploadAssetsDirectory = `${appDataPath}/${assetsDirectory}`
